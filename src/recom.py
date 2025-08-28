5c0858d382080a972ff8f2a555f3f46763ca1c24
Marcell Suyanto <marscell@connect.hku.hk>
2025-08-28 11:40:09 +0800

    Implemented Cosine Similarity Feature

--- src/recom.py
+++ src/recom.py
@@ -1,12 +1,20 @@
 import yaml
-from get_data import *
+from get_data import get_config
 import re
 import spacy
 from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+MODEL = "all-MiniLM-L6-v2"
+print(f"Model in use: {MODEL}")
+print("Setting up model and configuration...")
 
 nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
-model_to_use = SentenceTransformer("all-MiniLM-L6-v2")
+model_to_use = SentenceTransformer(MODEL) # all-mpnet-base-v2, best quality # all-MiniLM-l6-v2, fast and good quality
 config = get_config()
+print(f"Model and configuration loaded")
+
 
 def clean_job_description(text):
     text = re.sub(r'<.*?>', '', text)
@@ -20,7 +28,12 @@ def clean_job_description(text):
             cleaned_tokens.append(token.lemma_)
     return " ".join(cleaned_tokens)
 
-raw_description = """
+raw_description = """Strong programming ability, and knowledge in hands-on coding with
+Machine Learning and Deep Learning paradigms;
+Aim to utilize the latest AI technology to Finance and Biology;
+Self discipline to learn and contribute in a research environment"""
+
+"""
 Oxbridge Economics is seeking a Marketing Associate who is creative, organized, and passionate about bridging strategic communication with compelling design. You will support our business development and client engagement efforts by producing high-quality marketing materials, coordinating events, and serving as a key liaison with potential clients and partners.
   Key Responsibilities Client Communication: Engage with prospective clients, partners, and stakeholders through email, meetings, and events to promote Oxbridge’s offerings.
 Marketing Content Creation:Design and prepare decks, posters, email campaigns, social media posts, and promotional videos to support product launches and brand visibility.
@@ -35,11 +48,28 @@ Event Coordination:Organize and execute marketing events, webinars, and campaign
  Prior experience in B2B marketing or working with startups is a bonus.
 """
 
-def process_input(job_title, job_desc, job_nature):
+def process_jobs(job_title, job_desc, job_nature):
+    # Change to batch encoding
     cleaned_desc = clean_job_description(job_desc)
-    input_text = "Position: " + job_title + ". Nature: " + job_nature +". Details: " + cleaned_desc
+    input_text = "Position: " + job_title + ". Industry: " + job_nature +". Details: " + cleaned_desc
     embeddings = model_to_use.encode(input_text)
-    
     return embeddings
 
-print(process_input("Marketing Associate", raw_description, "Full-time").shape)
+def process_input(job_title, skills, industry):
+    input_text = "Position: " + job_title + ". Skills: " + ", ".join(skills) + ". Industry: " + industry
+    print("================================")
+    print(f"Position: {job_title}")
+    print(f"Skills: {', '.join(skills)}")
+    print(f"Industry: {industry}")
+    embeddings = model_to_use.encode(input_text)
+    return embeddings
+
+
+def compare_embeddings(embeddings1, embeddings2):
+    print(f"Comparing embeddings...")
+    return cosine_similarity([embeddings1], [embeddings2])[0][0]
+
+job_embeddings = process_jobs("Deep Learning Intern", raw_description, "IT/Programming")
+user_embeddings = process_input("Marketing Associate", ["marketing", "communication", "design"], "Marketing/Finance/Business")
+
+print(f"Similarity Score: {compare_embeddings(job_embeddings, user_embeddings)}")
